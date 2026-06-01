6aaa39c780aadfcb053700a380cd6bceb41e48f4
hyuk-dev <hyuk.development@gmail.com>
2026-06-01 09:56:32 +0900

    github actions runner에서 빌드 후 ì ec2로 전송(scp-action)

--- src/main/java/web/mvc/cicd02_boot/controller/MainController.java
+++ src/main/java/web/mvc/cicd02_boot/controller/MainController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class MainController {
     @GetMapping("/")
     public String index(){
-        return "GitHub Actions를 이용한 CI/CD 테스트입니다. - 자동 배포중, application.properties 자동 세팅";
+        return "GitHub Actions - 6월의 시작";
     }
 }
