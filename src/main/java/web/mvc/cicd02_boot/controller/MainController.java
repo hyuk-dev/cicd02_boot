83449f448428fbc850ce245e5957c58fddb86f78
hyuk-dev <hyuk.development@gmail.com>
2026-06-01 11:09:29 +0900

    Docker를  활용한 자동배포

--- src/main/java/web/mvc/cicd02_boot/controller/MainController.java
+++ src/main/java/web/mvc/cicd02_boot/controller/MainController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class MainController {
     @GetMapping("/")
     public String index(){
-        return "GitHub Actions - 6월의 시작";
+        return "GitHub Actions - Docker compose 테스트";
     }
 }
